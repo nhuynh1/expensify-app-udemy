dfdd68c754449d5ff1ee775185f7dc4046ad0710
Nancy <nhuynh@gmail.com>
2020-07-17 15:35:12 -0400

    Update firebase.js

--- src/firebase/firebase.js
+++ src/firebase/firebase.js
@@ -3,7 +3,8 @@ import * as firebase from 'firebase';
 const firebaseConfig = {
     apiKey: process.env.FIREBASE_API_KEY,
     authDomain: process.env.FIREBASE_AUTH_DOMAIN,
-    databaseURL: 'https://expensify-b14f6.firebaseio.com',
+    //databaseURL: 'https://expensify-b14f6.firebaseio.com',
+    databaseURL: process.env.FIREBASE_DATABASE_URL,
     projectId: process.env.FIREBASE_PROJECT_ID,
     storageBucket: process.env.FIREBASE_STORAGE_BUCKET,
     messagingSenderId: process.env.FIREBASE_MESSAGING_SENDER_ID,
