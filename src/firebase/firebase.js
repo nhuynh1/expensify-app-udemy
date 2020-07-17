ae5a22ea406f1fa859170ec9b19c2e6ea72f180e
Nancy Huynh <nhuynh@gmail.com>
2020-07-17 15:51:32 -0400

    webpack fixes

--- src/firebase/firebase.js
+++ src/firebase/firebase.js
@@ -3,7 +3,6 @@ import * as firebase from 'firebase';
 const firebaseConfig = {
     apiKey: process.env.FIREBASE_API_KEY,
     authDomain: process.env.FIREBASE_AUTH_DOMAIN,
-    //databaseURL: 'https://expensify-b14f6.firebaseio.com',
     databaseURL: process.env.FIREBASE_DATABASE_URL,
     projectId: process.env.FIREBASE_PROJECT_ID,
     storageBucket: process.env.FIREBASE_STORAGE_BUCKET,
@@ -11,7 +10,6 @@ const firebaseConfig = {
     appId: process.env.FIREBASE_APP_ID
 };
 
-console.log(process.env.FIREBASE_DATABASE_URL);
 firebase.initializeApp(firebaseConfig);
 
 const database = firebase.database();
